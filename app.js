edeff7c3410ebb704c5cc9535ea6ad4459c09c12
masoud moradi <massi.parser.code@gmail.com>
2022-10-21 12:36:41 -0700

    layouts views with block

--- app.js
+++ app.js
@@ -10,5 +10,9 @@ app.set("view engine", "pug");
 app.set("views", "views");
 
 app.get("/", (req, res, next) => {
-  res.status(200).render("home");
+  let payload = {
+    pageTitle: "Home",
+  };
+
+  res.status(200).render("home", payload);
 });
